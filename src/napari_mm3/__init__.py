48db8d3a14bb63fe9dab36de4bcb79c4c81b7b23
Michael Sandler <msandler@ucsd.edu>
2022-08-03 23:46:23 -0700

    Move segment_otsu into its own file

--- src/napari_mm3/__init__.py
+++ src/napari_mm3/__init__.py
@@ -5,3 +5,6 @@ __version__ = "0.0.6"
 from ._annotate import Annotate
 from ._channel_picker import ChannelPicker
 from ._compile import Compile
+from ._subtract import Subtract
+from ._track import Track
+from ._segment_otsu import SegmentOtsu
