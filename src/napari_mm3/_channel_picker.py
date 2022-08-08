1a3f67ab668396a394227442c2323c0ffc7a4fa5
Michael Sandler <msandler@ucsd.edu>
2022-08-08 16:18:16 -0700

    Update channel picker UI to contain more information

--- src/napari_mm3/_channel_picker.py
+++ src/napari_mm3/_channel_picker.py
@@ -1,11 +1,18 @@
 import os
+import pickle
 from pathlib import Path
+from turtle import shape
 import magicgui
 import napari
 import numpy as np
 import yaml
 import tifffile as tiff
 
+
+TRANSLUCENT_RED = np.array([1., 0., 0., .25])
+TRANSLUCENT_GREEN = np.array([0., 1., 0., .25])
+TRANSLUCENT_BLUE = np.array([0., 0., 1., .25])
+TRANSPARENT = np.array([0, 0, 0, 0])
 
 def load_fov(image_directory, fov_id):
     print("getting files")
@@ -28,6 +35,13 @@ def load_fov(image_directory, fov_id):
     print("numpying files")
     return np.array(image_fov_stack)
 
+def load_crosscorrs(analysis_directory, fov_id):
+    print("Getting crosscorrs")
+    with (analysis_directory/"crosscorrs.pkl").open("rb") as data:
+        cross_corrs = pickle.load(data)
+    fov_crosscorrs = cross_corrs[fov_id]
+    average_crosscorrs = {peak:fov_crosscorrs[peak]['cc_avg'] for peak in fov_crosscorrs}
+    return average_crosscorrs
 
 @magicgui.magic_factory(
     auto_call=True,
@@ -50,16 +64,14 @@ def ChannelPicker(
     analysis_directory: str = "analysis",
     cur_fov: int = 1,
 ):
-    """
-    TODO: Add visual feedback with xcorrs included!
-    """
     specs = None
-
     with (data_directory / analysis_directory / "specs.yaml").open("r") as specs_file:
         specs = yaml.safe_load(specs_file)
     if specs == None:
-        print("Error: No specs file")
-        return
+        file_location = data_directory / analysis_directory / "specs.yaml"
+        raise FileNotFoundError(
+            f"Specs file not found. Looked for it in the following location:\n {file_location.absolute().as_posix()}"
+        )
 
     fov_id_list = list(specs.keys())
     if cur_fov not in fov_id_list:
@@ -68,40 +80,64 @@ def ChannelPicker(
         )
     fov_id = cur_fov
 
-    image_fov_stack = load_fov(data_directory / image_directory, fov_id)
-
-    print("Rendering image")
+    # Set up viewer.
     viewer.layers.clear()
     viewer.grid.enabled = False
+    viewer.text_overlay.text = (
+        "Interactive channel picker. Click to change channel mode. "
+        "Color code:\n" 
+        "    Green: A channel with bacteria, \n" 
+        "    Blue: An empty channel without bacteria, to be used as a template. \n" 
+        "    Red: A channel to ignore. \n" 
+        "The number above the channel is the crosscorrelation. \n"
+        "A higher value means that that the channel is more likely to be empty."
+        )
+    viewer.text_overlay.visible = True
+    viewer.text_overlay.color = 'white'
+
+    print("Rendering images")
+    image_fov_stack = load_fov(data_directory / image_directory, fov_id)
     images = viewer.add_image(np.array(image_fov_stack))
-    sorted_peaks = list(sorted(specs[fov_id].keys()))
-    sorted_specs = [specs[fov_id][p] for p in sorted_peaks]
     viewer.dims.current_step = (0, 0)
     images.reset_contrast_limits()
     images.gamma = 0.5
 
-    ## get height of fov stack
+    # Set up selection box dimensions
+    sorted_peaks = list(sorted(specs[fov_id].keys()))
+    sorted_specs = [specs[fov_id][p] for p in sorted_peaks]
     height = image_fov_stack.shape[2]
     width = image_fov_stack.shape[3] / len(sorted_peaks)
-
     spread = width // 2
-    ## get tiff height and width
     coords = [[[0, p - spread], [height, p + spread]] for p in sorted_peaks]
 
-    ## add list of colors for each rectangle... this should really be an enum
-    spec_to_color = {
-        -1: "red",
-        0: "blue",
-        1: "green",
+    # Set up crosscorrelation text
+    crosscorrs = load_crosscorrs(data_directory / analysis_directory, cur_fov)
+    properties = {"peaks": sorted_peaks, "crosscorrs": crosscorrs.values()}
+    text_parameters = {
+        'text': "{crosscorrs:.03f}",
+        'size': 8,
+        'anchor': 'upper_left',
+        'visible': True,
+        'color': 'white'
     }
 
+    # Set box colors
+    spec_to_color = {
+        -1: TRANSLUCENT_RED,
+        0: TRANSLUCENT_BLUE,
+        1: TRANSLUCENT_GREEN,
+    }
     curr_colors = [spec_to_color[n] for n in sorted_specs]
+
+    # Add channel boxes.
     shapes_layer = viewer.add_shapes(
         coords,
         shape_type="rectangle",
         face_color=curr_colors,
-        properties=sorted_peaks,
-        opacity=0.25,
+        edge_color=TRANSPARENT,
+        properties=properties,
+        text=text_parameters,
+        opacity=1,
     )
 
     @shapes_layer.mouse_drag_callbacks.append
