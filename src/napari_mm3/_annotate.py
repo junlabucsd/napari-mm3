101375b0b48527cd4e295acabdde34289d9e75d2
Michael Sandler <msandler@ucsd.edu>
2022-08-22 18:45:21 -0700

    Create mm3 container widget. Use it for FOV picking.

--- src/napari_mm3/_annotate.py
+++ src/napari_mm3/_annotate.py
@@ -3,24 +3,110 @@ import numpy as np
 import tifffile as tiff
 import yaml
 import os
+import re
+from pathlib import Path
+from napari import Viewer
 
 
-class Annotate(Container):
+class MM3Container(Container):
     def __init__(self, napari_viewer):
         super().__init__()
+        # TODO: Remove 'reload data' button. Make it all a bit more dynamic.
         self.viewer = napari_viewer
-        self.data_directory_widget = FileEdit(mode="d", label="data directory")
-        self.data_directory_widget.tooltip = (
-            "Directory within which all your data and analyses will be located."
+
+        self.data_directory_widget = FileEdit(
+            mode="d",
+            label="data directory",
+            value=Path("."),
+            tooltip="Directory within which all your data and analyses will be located.",
         )
-        # TODO: Make this auto-inferred from a function type signature.
-        self.experiment_name_widget = LineEdit(label="output prefix")
-        self.experiment_name_widget.tooltip = (
-            "Optional. A prefix that will be prepended to output files."
+        self.data_directory_widget.changed.connect(self.set_data_directory)
+        self.set_data_directory()
+        self.append(self.data_directory_widget)
+
+        self.analysis_folder_widget = FileEdit(
+            mode="d",
+            label="analysis folder",
+            tooltip="Required. Location (within working directory) for outputting analysis. If in doubt, leave as default.",
+            value=Path("./analysis"),
         )
-        self.analysis_folder_widget = LineEdit(label="analysis folder")
-        self.analysis_folder_widget.tooltip = "Required. Location (within working directory) for outputting analysis. 'working directory/analysis/' by default."
-        self.analysis_folder_widget.value = "analysis"
+        self.analysis_folder_widget.changed.connect(self.set_analysis_folder)
+        self.set_analysis_folder()
+        self.append(self.analysis_folder_widget)
+
+        self.TIFF_folder_widget = FileEdit(
+            mode="d",
+            label="TIFF folder",
+            tooltip="Required. Location (within working directory) for the input images. If in doubt, leave as default.",
+            value=Path("./TIFF"),
+        )
+        self.TIFF_folder_widget.changed.connect(self.set_TIFF_folder)
+        # Automatically try to set the TIFF folder from the default.
+        self.set_TIFF_folder()
+        self.append(self.TIFF_folder_widget)
+
+        self.experiment_name_widget = LineEdit(
+            label="output prefix",
+            tooltip="Optional. A prefix that will be prepended to output files. If in doubt, leave blank.",
+        )
+        self.experiment_name_widget.changed.connect(self.set_experiment_name)
+        self.set_experiment_name()
+        self.append(self.experiment_name_widget)
+
+        self.load_data_widget = PushButton(
+            label="reload data",
+            tooltip="Load data from specified directories.",
+        )
+        self.load_data_widget.clicked.connect(self.set_valid_fovs)
+        self.set_valid_fovs()
+        self.append(self.load_data_widget)
+
+    def set_data_directory(self):
+        self.data_directory = self.data_directory_widget.value
+
+    def set_analysis_folder(self):
+        self.analysis_folder = self.analysis_folder_widget.value
+
+    def set_experiment_name(self):
+        self.experiment_name = self.experiment_name_widget.value
+
+    def set_TIFF_folder(self):
+        self.TIFF_folder = self.TIFF_folder_widget.value
+
+    def set_valid_fovs(self):
+        self.fovs = self.get_valid_fovs(self.TIFF_folder)
+
+    def get_valid_fovs(self, TIFF_folder):
+        found_files = TIFF_folder.glob("*.tif")
+        filenames = [f.name for f in found_files]
+        get_fov_regex = re.compile("xy(\d*)")
+        fov_strings = set(get_fov_regex.findall(filename)[0] for filename in filenames)
+        fovs = map(int, sorted(fov_strings))
+        return list(fovs)
+
+
+class SingleFOVChooser(SpinBox):
+    def __init__(self, permitted_FOVS):
+        min_FOV = min(permitted_FOVS)
+        max_FOV = max(permitted_FOVS)
+        label_str = f"FOV ({min_FOV}-{max_FOV})"
+        super().__init__(
+            label=label_str,
+            tooltip="The FOV you would like to annotate.",
+            min=min_FOV,
+            max=max_FOV,
+        )
+
+
+class FOVChooser:
+    def __init__(self, permitted_FOVs):
+        pass
+
+
+class Annotate(MM3Container):
+    def __init__(self, napari_viewer: Viewer):
+        super().__init__(napari_viewer)
+
         self.next_peak_widget = PushButton(label="next peak")
         self.next_peak_widget.tooltip = (
             "Jump to the next peak (typically the next channel)"
@@ -29,33 +115,25 @@ class Annotate(Container):
         self.prior_peak_widget.tooltip = (
             "Jump to the previous peak (typically the previous channel)"
         )
-        self.FOV_id_widget = SpinBox(label="FOV")
-        self.FOV_id_widget.tooltip = "The FOV you would like to annotate."
+        self.FOV_id_widget = SingleFOVChooser(self.fovs)
         self.FOV_id_widget.value = 1
 
         self.peak_id = 0
         # Need this to detect *changes* in the FOV_id_widget.
         self.FOV_id = 1
 
-        self.data_directory_widget.changed.connect(self.load_data)
-        self.analysis_folder_widget.changed.connect(self.load_data)
         self.next_peak_widget.clicked.connect(self.next_peak)
         self.prior_peak_widget.clicked.connect(self.prior_peak)
         self.FOV_id_widget.changed.connect(self.FOV_id_changed)
 
-        self.insert(0, self.data_directory_widget)
-        self.insert(1, self.analysis_folder_widget)
-        self.insert(2, self.experiment_name_widget)
-        self.insert(3, self.next_peak_widget)
-        self.insert(4, self.prior_peak_widget)
-        self.insert(5, self.FOV_id_widget)
+        self.append(self.next_peak_widget)
+        self.append(self.prior_peak_widget)
+        self.append(self.FOV_id_widget)
+
+        self.load_data()
 
     def load_specs(self):
-        with (
-            self.data_directory_widget.value
-            / self.analysis_folder_widget.value
-            / "specs.yaml"
-        ).read("r") as specs_file:
+        with (self.analysis_folder / "specs.yaml").open(mode="r") as specs_file:
             return yaml.safe_load(specs_file)
 
     def get_cur_fov(self, specs):
@@ -71,18 +149,15 @@ class Annotate(Container):
         fov = self.get_cur_fov(specs)
         peak = self.get_cur_peak(specs)
 
-        data_directory = self.data_directory_widget.value
-        experiment_name = self.experiment_name_widget.value
+        experiment_name = self.experiment_name
 
         img_filename = (
-            data_directory
-            / self.analysis_folder_widget.value
+            self.analysis_folder
             / "channels"
             / f"{experiment_name}_xy{fov:03d}_p{peak:04d}_c1.tif"
         )
         mask_filename = (
-            data_directory
-            / self.analysis_folder_widget.value
+            self.analysis_folder
             / "segmented"
             / f"{experiment_name}_xy{fov:03d}_p{peak:04d}_seg_unet.tif"
         )
@@ -131,8 +206,7 @@ class Annotate(Container):
 
         labels = self.viewer.layers[1].data.astype(np.uint8)
         fileout_name = (
-            training_dir
-            / f"{self.experiment_name_widget.value}_xy{fov:03d}_p{peak:04d}_seg.tif"
+            training_dir / f"{self.experiment_name}_xy{fov:03d}_p{peak:04d}_seg.tif"
         )
         tiff.imsave(fileout_name, labels)
         print("Training data saved")
