38e8111e2656736aa9080b56c35bede0774fcf8c
Michael Sandler <msandler@ucsd.edu>
2022-08-29 12:42:57 -0700

    Disable otsu seg import in __init__

--- src/napari_mm3/__init__.py
+++ src/napari_mm3/__init__.py
@@ -6,5 +6,6 @@ from ._channel_picker import ChannelPicker
 from ._compile import Compile
 from ._subtract import Subtract
 from ._track import Track
-from ._segment_otsu import SegmentOtsu
+# TODO: Re-enable later.
+# from ._segment_otsu import SegmentOtsu
 from ._segment_unet import SegmentUnet
