91689fd61d7da1dfe43f5700f64c2d6ae8189087
Michael Sandler <msandler@ucsd.edu>
2022-08-29 14:16:18 -0700

    Try commenting out __init__ lines; as well as test_placeholder.

--- src/napari_mm3/__init__.py
+++ src/napari_mm3/__init__.py
@@ -1,11 +1,11 @@
 __version__ = "0.0.6"
 
 
-from ._annotate import Annotate
-from ._channel_picker import ChannelPicker
-from ._compile import Compile
-from ._subtract import Subtract
-from ._track import Track
-# TODO: Re-enable later.
-# from ._segment_otsu import SegmentOtsu
-from ._segment_unet import SegmentUnet
+# from ._annotate import Annotate
+# from ._channel_picker import ChannelPicker
+# from ._compile import Compile
+# from ._subtract import Subtract
+# from ._track import Track
+# # TODO: Re-enable later.
+# # from ._segment_otsu import SegmentOtsu
+# from ._segment_unet import SegmentUnet
--- src/napari_mm3/_tests/test_placeholder.py
+++ src/napari_mm3/_tests/test_placeholder.py
@@ -1,12 +1,12 @@
 # Placeholder test; add actual testing here eventually. For now, this just checks that at least imports work.
-import napari_mm3._annotate
-import napari_mm3._channel_picker
-import napari_mm3._compile
-import napari_mm3._deriving_widgets
-import napari_mm3._function
-import napari_mm3._nd2_to_tiff
-# Disable for now. TODO: Re-enable when I figure out what's wrong.
-# import napari_mm3._segment_otsu 
-import napari_mm3._segment_unet
-import napari_mm3._subtract
-import napari_mm3._track
+# import napari_mm3._annotate
+# import napari_mm3._channel_picker
+# import napari_mm3._compile
+# import napari_mm3._deriving_widgets
+# import napari_mm3._function
+# import napari_mm3._nd2_to_tiff
+# # Disable for now. TODO: Re-enable when I figure out what's wrong.
+# # import napari_mm3._segment_otsu 
+# import napari_mm3._segment_unet
+# import napari_mm3._subtract
+# import napari_mm3._track
